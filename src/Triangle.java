f8f87a80296bbd6a0e03d42892af94528b4e28c3
Jerin Prakash <jerinprakash892@gmail.com>
2026-01-11 12:38:42 +0530

    Added the code

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,14 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args) {
+        for (int i = 1; i <=3; i++) {
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print("*");
+                if(j!=2*i-1){
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
 }
